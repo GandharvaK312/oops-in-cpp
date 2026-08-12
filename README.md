fea0121bf06c882a367a97c3c4da296b61325ae2
GandharvaK312 <gandharvak2021@gmail.com>
2026-08-13 04:30:58 +0530

    more constructors and types of constructors

--- README.md
+++ README.md
@@ -29,7 +29,30 @@ Scope/access specifiers:
 - data hiding (using private to store data that should not be accessile to the general user base)
 - hiding passwords and account balances in a bank application
 
-`Constructors` : a special method that is created when a class is declared. 
+`Constructors` : a special method that is automatically invoked when an object is declared. Used for initialization of objects.
+- same name as classs
+- no return type
+- only called once during object creation
+- memory allocation happens when constructor is called
+- always declared in public scope
+
+Three types of constructors:
+- `non parameterized`: does not use parameters
+- `parameterized`: uses parameters
+- `copy`: special constructor that copies properties of one object to another (made by compiler default even if user does not)
+    ``` cpp
+        className obj1(parameters);
+        className obj2(obj1);
+    ```
+    types of copy:
+        - `shallow copy`: copies all the member values from one object to another (majority use case) problematic when Dynamic memory allocation invovled
+        - `deep copy`: makes copies of member values and also copies of dynamically allocated memory the member points to
+
+all three types having the same name (as the class name) can be used in the same program but with different types of parameters. This feature of a constructor to be defined in different ways based on parameters is called `Constructor Overloading` and is an example of `Polymorphism`
+
+"this -> " is a special pointer in C++ used to point to the current pointer and its properties and methods. Used when programmer wants to pass the parameteres with the same name as properties so uses this->properties to refer to the objects properties.
+
+Alternativly `this -> property` can be written as `*(this).prop`
 
 `Abstraction`
 
--- class declaration.cpp
+++ class declaration.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <ostream>
 #include <string>
 using namespace std;
 
@@ -10,6 +11,36 @@ private: // important / private information should be under private
 
 public:
 	string name, dept, subject;
+	
+	// non-parameterized contructor
+	Teacher(){ //always declared in public scope
+//		cout << "Hi, I am constructor\n"; // manual initialization of constructor (avoidable btw)
+        	dept = "comp sci";
+	}
+
+	// parameterized constructor
+	// teacher constructor passed with temporary params
+
+//	Teacher(string n, string d, string s, double sal){
+//		name = n; dept = d; s = subject; salary = sal;
+//	}
+
+	// teacher constructor passed with parameters same as properties
+	Teacher(string name, string dept, string subject, double salary){
+		this -> name = name; //this is used to refer to the object's "name" property
+		this -> dept = dept;
+		this -> subject = subject;
+		this -> salary = salary;
+	}
+
+	// Shallow copy: Manual declaration and definition of copy constructor
+	Teacher(Teacher &orgObj){ // pass by reference
+//		cout << "I am a custom copy constructor\n";
+		this -> name = orgObj.name;
+		this -> subject = orgObj.subject;
+		this -> dept = orgObj.dept;
+		this -> salary = orgObj.salary;
+	}
 
 	//methods / member functions
 	void changeDept(string newDept){
@@ -25,25 +56,46 @@ public:
 	double getSalary(){
 		return salary;
 	}
+
+	void getInfo(){
+		cout << "name: " << name << endl;
+		cout << "subject: " << subject << endl;
+		cout << "dept: " << dept << endl;
+		cout << "Salary: " << salary << endl;
+	}
 };
 
 class Student {
 public:
-	string name; int age, rollno;
+	string name; double cgpa;
+
+	Student(string name, double cgpa){
+		this -> name = name;
+		this -> cgpa = cgpa;
+	}
+
+	void getInfo(){
+		cout << "name: " << name << endl;
+		cout << "cgpa: " << cgpa << endl;
+	}
 };
 
 int main(void){
 
 	// Object declaration
-	Teacher t1; //can be one or multiple
+	Teacher t1("Murti", "cs", "digital logic", 12000); //can be one or multiple. Constructor is called.
 	
 	t1.name = "Raj";
 	t1.dept = "Comp Sci";
 	t1.subject = "C++";
 	t1.setSalary(250000);
 
+	// cout << t1.dept << endl;
+//	t1.getInfo();
+	Teacher t2(t1); //default invoked copy constructor copied properties of t1 into t2.
+	// t2.getInfo();
 
-	cout << t1.name << endl;
-	cout << t1.getSalary() << endl;
+	Student s1("rahul kumar", 9.9);
+	s1.getInfo();
 	return 0;
 }
